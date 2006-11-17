c04076767a945ba324706e709aa4332d40726c9e
alastair-irving <29164966+ajirving@users.noreply.github.com>
2006-11-17 21:23:21 +0000

    Initial commit.

---
+++ latex_access_com.py
@@ -0,0 +1,25 @@
+#This file defines a com object wihch can be used to interface between 
+#screenreaders and the latex access scripts
+
+import latex_access
+import nemeth
+
+
+class latex_access_com:
+    _reg_clsid_ = "{436BC4EC-405C-49ED-A0E7-84945B0BAC03}"
+    _reg_progid_ = "latex_access"
+    _public_methods_ =["nemeth","nemeth_add"]
+    def nemeth(self, input):
+        '''Translates the input into Nemeth Braille.'''
+        return latex_access.translate(str(input),nemeth.nemeth_table)
+
+
+def nemeth_add(input,translation,self):
+    '''A function to add entries to the nemeth dictionary.'''
+    nemeth.nemeth_table[str(input)]=str(translation)
+
+#Register the object
+if __name__=='__main__':
+    import pythoncom,win32com.server.register
+    latex_access_com._reg_clsid=pythoncom.CreateGuid()
+    win32com.server.register.UseCommandLine(latex_access_com)
