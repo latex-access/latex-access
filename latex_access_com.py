aa37cd75ff460197b7de04411c1ec5a2d7baf246
alastair-irving <29164966+ajirving@users.noreply.github.com>
2007-07-18 20:34:58 +0000

    Added speech support to com object.

--- latex_access_com.py
+++ latex_access_com.py
@@ -4,18 +4,22 @@
 import latex_access
 import preprocessor
 import nemeth
-
+import speech
 
 class latex_access_com:
     _reg_clsid_ = "{436BC4EC-405C-49ED-A0E7-84945B0BAC03}"
     _reg_progid_ = "latex_access"
-    _public_methods_ =["nemeth","preprocessor_add","load_csv"]
+    _public_methods_ =["nemeth","speech","preprocessor_add","load_csv"]
     def nemeth(self, input):
         '''Translates the input into Nemeth Braille.'''
         input=preprocessor.process(str(input))
         return latex_access.translate(input,nemeth.table)
-
-
+    
+    def speech(self,input):
+        '''Translates the input into english speech.'''
+        input=preprocessor.process(str(input))
+        return latex_access.translate(input,speech.table)
+    
     def preprocessor_add(self,input,translation):
         '''A function to add entries to the preprocessor'''
         preprocessor.table[str(input)]=str(translation)
