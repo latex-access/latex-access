450173ff729eea20db252f7f7ec341f618acb868
alastair-irving <29164966+ajirving@users.noreply.github.com>
2008-06-18 14:37:17 +0000

    Added dollar toggling support ot the com object.

--- latex_access_com.py
+++ latex_access_com.py
@@ -10,7 +10,7 @@ class latex_access_com:
     def __init__(self):
         self._reg_clsid_ = "{436BC4EC-405C-49ED-A0E7-84945B0BAC03}"
         self._reg_progid_ = "latex_access"
-        self._public_methods_ =["nemeth","speech","preprocessor_add","load_csv"]
+        self._public_methods_ =["nemeth","speech","preprocessor_add","load_csv","toggle_dollars_nemeth","toggle_dollars_speech"]
     def nemeth(self, input):
         '''Translates the input into Nemeth Braille.'''
         input=preprocessor.process(str(input))
@@ -20,7 +20,29 @@ class latex_access_com:
         '''Translates the input into english speech.'''
         input=preprocessor.process(str(input))
         return latex_access.translate(input,speech.table)
-    
+
+    def toggle_dollars_nemeth(self):
+        '''Toggles whether dollars are shown in braille.
+
+        Returns a boolian of whether dollars are being removed.'''
+        if(nemeth.remove_dollars):
+            nemeth.remove_dollars=False
+            return False
+        else:
+            nemeth.remove_dollars=True
+            return True
+
+    def toggle_dollars_speech(self):
+        '''Toggles whether dollars are spken 
+
+        Returns a boolian of whether dollars are being removed.'''
+        if(speech.remove_dollars):
+            speech.remove_dollars=False
+            return False
+        else:
+            speech.remove_dollars=True
+            return True
+
     def preprocessor_add(self,input,translation):
         '''A function to add entries to the preprocessor'''
         preprocessor.table[str(input)]=str(translation)
