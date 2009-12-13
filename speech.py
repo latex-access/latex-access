88950e579db6063517783e6e38553a2306b2d70c
alastair-irving <alastair-irving@04194296-f821-0410-bbb1-fcfd4500bfb0>
2009-12-13 12:50:16 +0000

    Added \prod to speech.

--- speech.py
+++ speech.py
@@ -26,7 +26,7 @@ class speech(latex_access.translator):
                    "\\infty":" infinity ","\\rightarrow":" goes to ","\\leftrightarrow":" if and only if ","\\Rightarrow":" goes to","\\Leftrightarrow":" if and only if ",
                    "\\sin":" sine ","\\cos":" coz ","\\tan":"tan","\\sinh":" shine ","\\cosh":" cosh ","\\tanh":" thann ",
                    "\\hat":(" "," hat "),"\\widehat":(" "," hat "),"\\bar":(" "," bar "),"\\overline":(" "," bar "),"\\dot":(" "," dot "),"\\ddot":(" "," double dot "),
-                   "\\partial":" partial ","\\nabla":" nabla ","\\wedge":" vec ","\\lim":" limit ","\\sum":" sum ","\\therefore":" therefore ","\\forall":" for all "}
+                   "\\partial":" partial ","\\nabla":" nabla ","\\wedge":" vec ","\\lim":" limit ","\\sum":" sum ","\\prod":" product ","\\therefore":" therefore ","\\forall":" for all "}
         for (k,v) in new_table.iteritems():
             self.table[k]=v        
 
