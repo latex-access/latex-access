a0f2cea7ccab5a1d57200d644bfc0ea75ad1e43b
ddalton10 <ddalton10@04194296-f821-0410-bbb1-fcfd4500bfb0>
2010-12-14 06:50:07 +0000

    Implement toggle of voicing/Brailling dollar signs for emacs. It reflects the change in toggle fine (see the echo area, after invoking the function), but dollar signs are always spoken. A friend believes he observes this behaviour with Jaws, I'll attempt to develop a fix perhaps using the .strip method of string. Currently no keybinding for this, just call interactively or make one yourself. I need to figure out the best way of doing the key bindings stuff without stealing already used keys.

--- latex_access_emacs.py
+++ latex_access_emacs.py
@@ -12,9 +12,12 @@ interact with classes."""
 
 import speech
 import nemeth 
+from latex_access_com import latex_access_com
 
 n=nemeth.nemeth()
 s=speech.speech()
+l=latex_access_com()
+
 def transbrl (arg):
   """Translate latex code into Nemeth Braile.
 
@@ -28,3 +31,19 @@ def transsp (arg):
   Unless calling with pymacs, please use
   speech.speech.translate(). Found in speech.py."""
   return s.translate(arg)
+
+def toggle_dollars_nemeth ():
+  """Toggle Brailling of dollar signs.
+
+  This function negotiates the class, because pymacs is challenging to
+  deal with."""
+
+  return l.toggle_dollars_nemeth()
+
+def toggle_dollars_speech ():
+  """Toggle the speaking of dollar signs.
+
+  This functions is here to negotiate the class, so that pymacs isn't
+  able to cause us trouble."""
+
+  return l.toggle_dollars_speech ()
