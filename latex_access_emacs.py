f400c0c202978f7ba75df7144310068eccd46b0f
alastair-irving <alastair-irving@04194296-f821-0410-bbb1-fcfd4500bfb0>
2010-12-14 21:04:06 +0000

    Added preprocessor support to latex_access_emacs.py

--- latex_access_emacs.py
+++ latex_access_emacs.py
@@ -12,16 +12,19 @@ interact with classes."""
 
 import speech
 import nemeth 
+import preprocessor
 
 n=nemeth.nemeth()
 s=speech.speech()
+p=preprocessor.preprocessor()
+nc=preprocessor.newcommands(p)
 
 def transbrl (arg):
   """Translate latex code into Nemeth Braile.
 
   Unless you are using pymacs to call this, please use the function
   nemeth.nemeth.translate() instead. Found in nemeth.py."""
-  return n.translate(arg)
+  return n.translate(p.translate(arg))
 
 def transsp (arg):
   """Translate a line of LaTeX source into understandable speech.
@@ -29,7 +32,7 @@ def transsp (arg):
   Unless calling with pymacs, please use
   speech.speech.translate(). Found in speech.py."""
 
-  return s.translate(arg)
+  return s.translate(p.translate(arg))
 
 def toggle_dollars_nemeth ():
   """Toggle Brailling of dollar signs.
@@ -47,4 +50,20 @@ def toggle_dollars_speech ():
   s.remove_dollars=not s.remove_dollars
   return s.remove_dollars
   
+
+
+
+def preprocessor_add(command,args,translation_string):
+  '''A function to add entries to the preprocessor'''
+  p.add_from_string(command,args,translation_string)
+
+def preprocessor_from_string(input):
+  '''Adds preprocessor entries from a LaTeX string containing \newcommand.'''
+  nc.translate(input)
+
+def preprocessor_write(filename):
+  p.write(filename)
+
+def preprocessor_read(filename):
+  p.read(filename)
 
