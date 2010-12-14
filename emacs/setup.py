9f12c03d4a0af261cc55aa65b390ca18eb4d44e8
ddalton10 <ddalton10@04194296-f821-0410-bbb1-fcfd4500bfb0>
2010-12-14 02:56:03 +0000

    Support byte-compiling. Consult emacs/README.emacs for details.

--- emacs/setup.py
+++ emacs/setup.py
@@ -11,13 +11,6 @@ import os
 
 defaultemacs = os.path.expanduser("~/.emacs") # default location of .emacs init file
 latexdir=os.path.realpath("../") # get dir above this one
-emacscode = (
-  "; Emacs latex-access:\n",
-  '(setq latex-access-path "'+latexdir+'")\n',
-  '(load (concat latex-access-path "/emacs/emacs-latex-access.el"))\n',
-  "(add-hook 'LaTeX-mode-hook 'latex-access-on) ; turn on when visiting a\n",
-  "; buffer in latex-mode \n",
-  "; End emacs Latex-access.\n")
 
 def do_install(initfile):
   """Install the relevant code into .emacs init file.
@@ -25,8 +18,33 @@ def do_install(initfile):
   This completes the installation of latex-access under emacs. pymacs
   should still be installed by the user. The relevant code must still be
   edited manually if either emacspeak or Braille is not present."""
+  byte=raw_input("Attempt to byte compile emacs-latex-access (Y/N)? ")
+  ext ='.elc' # do we use compiled or source file
+  if byte.lower() == 'y':
+    try:
+      os.system("emacs -batch -f batch-byte-compile"\
+                  " emacs-latex-access.el")
+    except:
+      print "Could not byte compile emacs-latex-access.el, continuing"\
+        " anyway with source installation.\nThis will still work fine!"
+      ext ='.el'
+  else: # don't compile
+    ext = '.el'
+    print "Not byte compiling..."
+  if ext == '.elc':
+    print "Byte compiled emacs-latex-access.el"
 
   print "Installing to %s" % (initfile)
+
+# Code to go in .emacs
+  emacscode = (
+  "; Emacs latex-access:\n",
+  '(setq latex-access-path "'+latexdir+'")\n',
+  '(load (concat latex-access-path "/emacs/emacs-latex-access'+ext+'"))\n',
+  "(add-hook 'LaTeX-mode-hook 'latex-access-on) ; turn on when visiting a\n",
+  "; buffer in latex-mode \n",
+  "; End emacs Latex-access.\n")
+
   try:
     f=open(initfile, "a") # append to user's .emacs
   except IOError:
