4c8d96326b6cab63fb6f548900c8dc3fc958cfe3
alastair-irving <29164966+ajirving@users.noreply.github.com>
2010-12-14 12:43:48 +0000

    Changed latex_access_emacs to use the correct means of dollar toggling.

--- latex_access_emacs.py
+++ latex_access_emacs.py
@@ -12,19 +12,15 @@ interact with classes."""
 
 import speech
 import nemeth 
-from latex_access_com import latex_access_com
 
 n=nemeth.nemeth()
 s=speech.speech()
-l=latex_access_com()
 
 def transbrl (arg):
   """Translate latex code into Nemeth Braile.
 
   Unless you are using pymacs to call this, please use the function
   nemeth.nemeth.translate() instead. Found in nemeth.py."""
-  if (l.nemeth_translator.remove_dollars):
-    return n.translate(arg).replace("$", "")
   return n.translate(arg)
 
 def transsp (arg):
@@ -33,8 +29,6 @@ def transsp (arg):
   Unless calling with pymacs, please use
   speech.speech.translate(). Found in speech.py."""
 
-  if(l.speech_translator.remove_dollars):
-    return s.translate(arg).replace("$", "")
   return s.translate(arg)
 
 def toggle_dollars_nemeth ():
@@ -42,13 +36,15 @@ def toggle_dollars_nemeth ():
 
   This function negotiates the class, because pymacs is challenging to
   deal with."""
-
-  return l.toggle_dollars_nemeth()
+  n.remove_dollars=not n.remove_dollars
+  return n.remove_dollars
 
 def toggle_dollars_speech ():
   """Toggle the speaking of dollar signs.
 
   This functions is here to negotiate the class, so that pymacs isn't
   able to cause us trouble."""
+  s.remove_dollars=not s.remove_dollars
+  return s.remove_dollars
+  
 
-  return l.toggle_dollars_speech ()
