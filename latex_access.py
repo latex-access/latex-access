7eae2efbe51075cfeb4ce1cac2a15448ab493de6
ddalton10 <7510833+danieldalton10@users.noreply.github.com>
2011-11-29 07:59:51 +0000

    Some of the settings code can be useful for nvda and jaws so move it to latex_access.py.

--- latex_access.py
+++ latex_access.py
@@ -287,3 +287,48 @@ def loadSettings (file):
     words = line.split()
     settings[words[0]] = words[1]
   f.close()
+
+def activateSettings (filename, instances):
+  """Activate settings stored in a file.
+
+  This function activates the settings in a file, which for the emacs
+  module is ~/.latex-access.
+
+  It also sets up the settings for active instances such as those as a
+  result of the nemeth class eg. nemeth.nemeth () and sets those active
+  sessions to the values specified in the config file. Note the
+  activation or deactivation of speech and Braille must be controlled by
+  each module independently, i.e. not here."""
+
+  try: # Settings file doesn't necessarily exist 
+    loadSettings (filename)
+  except: 
+    return False
+
+# Now convert settings in the dictionary into the proper variables 
+
+  instances["speech"].remove_dollars = not booleaniseSetting("speechdollars")
+  instances["braille"].remove_dollars = not booleaniseSetting("brailledollars")
+  return True # Settings activated 
+
+def booleaniseSetting (setting):
+  """Turn a setting value into a boolean type.
+  
+  As settings read from the config file are of type string, return a
+  boolean representation of this. 'true' or 'True' = True, while any
+  other string is False."""
+  if str(setting).lower () == 'true':
+    return True
+  else:
+    return False
+
+def getSetting (setting):
+  """Get the value of setting.
+
+  This function searches for the particular setting in the settings
+  dict, and if found, returns the settings' value."""
+
+  if setting in settings.keys():
+    return booleaniseSetting(settings[setting])
+  else: # setting not found
+    return False
