74df12eb6fe00b8844d26bb59a388e2cb62c7614
alastair-irving <29164966+ajirving@users.noreply.github.com>
2014-05-03 20:16:15 +0000

    Changed the indentation to 4 spaces in two files to make them the same as the rest of the project.

--- latex_access/settings.py
+++ latex_access/settings.py
@@ -14,104 +14,104 @@
 
 import os
 import ueb
-import nemeth 
+import nemeth
 
 globals
-# Global settings for latex-access, these are the default values 
+# Global settings for latex-access, these are the default values
 settings={"brailledollars":"True","speakdollars":"True","brailletable":"nemeth","capitalisation":"6dot","preprocessorfile":"~/.latex-access-preprocessor.strings", "speechfile":"","nemethfile":"","uebfile":""}
 
 def loadSettings (file):
-  """Read settings from file.
+    """Read settings from file.
 
-  This function reads the setting values from file. The settings are
-  saved in the public dict settings. The file should be in the form
-  settingname value
+    This function reads the setting values from file. The settings are
+    saved in the public dict settings. The file should be in the form
+    settingname value
 
-  Where settingname is a valid setting and value is the value of that
-  setting. The file may be commented by use of ; but only at the start
-  of a line! Blank lines are ignored."""
-  f=None
-  try:
-    f=open(file, "r")
-  except:
-    return False
-  else:
-    for line in f.readlines ():
-      if line[0] == "\n" or line[0] == ";": # Skip some irrelevant stuff
-        continue 
-      words = line.split()
-      if not words: # Ignore lines with spaces
-        continue 
-      if len(words) < 2:
-        continue 
-      settings[words[0]] = words[1].lower()
-    return True
-  finally:
-    if f:
-      f.close()
+    Where settingname is a valid setting and value is the value of that
+    setting. The file may be commented by use of ; but only at the start
+    of a line! Blank lines are ignored."""
+    f=None
+    try:
+        f=open(file, "r")
+    except:
+        return False
+    else:
+        for line in f.readlines ():
+            if line[0] == "\n" or line[0] == ";": # Skip some irrelevant stuff
+                continue
+            words = line.split()
+            if not words: # Ignore lines with spaces
+                continue
+            if len(words) < 2:
+                continue
+            settings[words[0]] = words[1].lower()
+        return True
+    finally:
+        if f:
+            f.close()
 
 def activateSettings (instances):
-  """Activate settings stored in a file.
+    """Activate settings stored in a file.
 
-  This function activates the settings in a file, which for the emacs
-  module is ~/.latex-access.
+    This function activates the settings in a file, which for the emacs
+    module is ~/.latex-access.
 
-  It also sets up the settings for active instances such as those as a
-  result of the nemeth class eg. nemeth.nemeth () and sets those active
-  sessions to the values specified in the config file. Note the
-  activation or deactivation of speech and Braille must be controlled by
-  each module independently, i.e. not here."""
+    It also sets up the settings for active instances such as those as a
+    result of the nemeth class eg. nemeth.nemeth () and sets those active
+    sessions to the values specified in the config file. Note the
+    activation or deactivation of speech and Braille must be controlled by
+    each module independently, i.e. not here."""
 
-  # points to our custom speech strings file 
-  speechfile =os.path.expanduser(settings["speechfile"])
-  # Decide what file holds Braille strings based on what table is in use.
-  if settings["brailletable"].lower() == "ueb":
-    bfile =os.path.expanduser(settings["uebfile"])
-  else:
-    bfile =os.path.expanduser(settings["nemethfile"])
-    
-  if 'speak' in instances.keys():
-    instances["speak"].remove_dollars = not booleaniseSetting("speakdollars")
-    if os.path.exists(speechfile) and os.path.isfile (speechfile):
-        instances["speak"].load_file(speechfile)
-  if 'braille' in instances.keys():
-    instances["braille"].remove_dollars = not booleaniseSetting("brailledollars")
-    instances["braille"].capitalisation=settings["capitalisation"]
-    if os.path.exists(bfile) and os.path.isfile (bfile):
-      instances["braille"].load_file(bfile)
-  if 'preprocessor' in instances.keys () and os.path.exists(os.path.expanduser (settings["preprocessorfile"])):
-    instances["preprocessor"].read(os.path.expanduser (settings["preprocessorfile"]))
+    # points to our custom speech strings file
+    speechfile =os.path.expanduser(settings["speechfile"])
+    # Decide what file holds Braille strings based on what table is in use.
+    if settings["brailletable"].lower() == "ueb":
+        bfile =os.path.expanduser(settings["uebfile"])
+    else:
+        bfile =os.path.expanduser(settings["nemethfile"])
 
-  return True # Settings activated 
+    if 'speak' in instances.keys():
+        instances["speak"].remove_dollars = not booleaniseSetting("speakdollars")
+        if os.path.exists(speechfile) and os.path.isfile (speechfile):
+            instances["speak"].load_file(speechfile)
+    if 'braille' in instances.keys():
+        instances["braille"].remove_dollars = not booleaniseSetting("brailledollars")
+        instances["braille"].capitalisation=settings["capitalisation"]
+        if os.path.exists(bfile) and os.path.isfile (bfile):
+            instances["braille"].load_file(bfile)
+    if 'preprocessor' in instances.keys () and os.path.exists(os.path.expanduser (settings["preprocessorfile"])):
+        instances["preprocessor"].read(os.path.expanduser (settings["preprocessorfile"]))
+
+    return True # Settings activated
 
 def booleaniseSetting (setting):
-  """Turn a setting value into a boolean type.
-  
-  As settings read from the config file are of type string, return a
-  boolean representation of this. 'true' or 'True' = True, while any
-  other string is False."""
-  if str(settings[setting]).lower () == 'true':
-    return True
-  else:
-    return False
+    """Turn a setting value into a boolean type.
+
+    As settings read from the config file are of type string, return a
+    boolean representation of this. 'true' or 'True' = True, while any
+    other string is False."""
+    if str(settings[setting]).lower () == 'true':
+        return True
+    else:
+        return False
 
 def getSetting (setting):
-  """Get the value of setting.
+    """Get the value of setting.
 
-  This function searches for the particular setting in the settings
-  dict, and if found, returns the settings' value."""
+    This function searches for the particular setting in the settings
+    dict, and if found, returns the settings' value."""
 
-  if setting in settings.keys():
-    return booleaniseSetting(setting)
-  else: # setting not found
-    return False
+    if setting in settings.keys():
+        return booleaniseSetting(setting)
+    else: # setting not found
+        return False
 
 def brailleTableToUse ():
-  """Return the instance of the braille module to use.
+    """Return the instance of the braille module to use.
 
-  This function return the instance of the Braille table that should be
-  used."""
-  if settings["brailletable"].lower() == "ueb":
-    return ueb.ueb()
-  else:
-    return nemeth.nemeth()
+    This function return the instance of the Braille table that should be
+    used."""
+    if settings["brailletable"].lower() == "ueb":
+        return ueb.ueb()
+    else:
+        return nemeth.nemeth()
