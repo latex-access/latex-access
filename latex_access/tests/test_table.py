05e1c3265453b4b85c4ee6b2f31b860abe899944
jakubl7545 <jakubl7545@gmail.com>
2022-03-29 21:37:08 +0200

    Add more tests for table module.

--- latex_access/tests/test_table.py
+++ latex_access/tests/test_table.py
@@ -22,3 +22,15 @@ class TestTable(unittest.TestCase):
             ),
             ["Name ", " Surname ", " Age   "]
         )
+
+    def test_where_am_i(self):
+        """Tests that information about current location in table is properly retrieved."""
+        self.assertEqual(table_module.WhereAmI('4&5', ['1', '2', '3'], '1&2&3\\\\4&5&6'), 'Focus is in column 2 at location B 2')
+
+    def test_get_table_top_row(self):
+        """Tests that table's top row is properly retrieved."""
+        self.assertEqual(table_module.GetTableTopRow('1&2&3\\\\4&5&6'), '1&2&3')
+
+    def test_get_table_current_row(self):
+        """Tests that table's current row is properly retrieved"""
+        self.assertEqual(table_module.GetTableCurrentRow('1&2&3\\\\4&5&6\\\\7&8&9'), '7&8&9')
